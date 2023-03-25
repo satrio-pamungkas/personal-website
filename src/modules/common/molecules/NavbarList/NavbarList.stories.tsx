2d688b33c08888374550502ee3c99e9a140aaadd
Muhammad Raihan Satrio <adewipo@gmail.com>
2023-03-25 20:23:11 +0700

    refactor: Story components

--- src/modules/common/molecules/NavbarList/NavbarList.stories.tsx
+++ src/modules/common/molecules/NavbarList/NavbarList.stories.tsx
@@ -7,11 +7,11 @@ export default {
 };
 
 const linkLabels = [
-  { id: 1, label: "Homepage", link: "/" },
-  { id: 2, label: "Projects", link: "/projects" },
-  { id: 3, label: "Publications", link: "/publications" },
-  { id: 4, label: "Resume", link: "/resume" },
-  { id: 5, label: "About Me", link: "/about" }
+  { id: 1, label: "Homepage", link: "/", active: true },
+  { id: 2, label: "Projects", link: "/projects", active: false },
+  { id: 3, label: "Publications", link: "/publications" , active: false },
+  { id: 4, label: "Resume", link: "/resume", active: false },
+  { id: 5, label: "About Me", link: "/about", active: false }
 ];
 
 const Template: ComponentStory<typeof NavbarList> = (args) => { 
