3b41ba18d9d69440b25ff8c3a7b95122ad949a7d
Muhammad Raihan Satrio <adewipo@gmail.com>
2023-03-29 18:15:15 +0700

    fix: font configuration and style

--- src/modules/common/molecules/NavbarList/NavbarList.tsx
+++ src/modules/common/molecules/NavbarList/NavbarList.tsx
@@ -13,9 +13,9 @@ interface INavbarList {
 
 export const NavbarList = (props: INavbarList) => {
   return (
-    <div className="items-center justify-between ">
+    <div className="items-center justify-between">
       <ul className="flex flex-col p-3 m-3 border md:flex-row text-sm
-        rounded-xl border-gray-500 bg-neutral-800 md:space-x-8 md:text-sm 
+        rounded-xl border-gray-500 bg-neutral-800 md:space-x-12 md:text-sm 
         md:font-medium md:border-0 md:bg-neutral-900"
       >
         {props.list.map((item) => (
