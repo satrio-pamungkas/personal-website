d7c7e4776fc664c36380ae15fa91b630681a2e5b
Muhammad Raihan Satrio <adewipo@gmail.com>
2023-04-03 13:29:44 +0700

    feat: added ButtonIcon component

---
+++ src/atoms/ButtonIcon/ButtonIcon.tsx
@@ -0,0 +1,13 @@
+import { IButtonIcon } from ".";
+
+export const ButtonIcon = (props: IButtonIcon) => {
+  return (
+    <button 
+      className="p-4 rounded-lg bg-neutral-600 
+        hover:bg-neutral-800"
+      data-testid="button-icon"
+    >
+      {props.icon}
+    </button>
+  );
+}
